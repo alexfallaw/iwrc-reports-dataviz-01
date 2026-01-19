cbdc3101060ce7b5380c1c059168d371c3adf478
alexfallaw <96636767+alexfallaw@users.noreply.github.com>
2026-01-19 17:24:58 -0600

    giving up on styling

--- base.py
+++ base.py
@@ -22,18 +22,16 @@ fig = plt.figure(figsize=(12, 6))
 # BAR GRAPH
 # create graph with x = funding type, y = project count
 
-with plt.style.context('seaborn-v0_8-pastel'):
-  ax1 = fig.add_subplot(1, 2, 1)
-  ax1.bar(fund_grps['Funding Type'], fund_grps['Project Count'])
-  ax1.set_title("Projects by Funding Type")
+ax1 = fig.add_subplot(1, 2, 1)
+ax1.bar(fund_grps['Funding Type'], fund_grps['Project Count'])
+ax1.set_title("Projects by Funding Type")
 
 # PIE CHART
 # create pie chart with funding type %, labels = funding type
 
-with plt.style.context('seaborn-v0_8-dark-palette'):
-  ax2 = fig.add_subplot(1, 2, 2)
-  ax2.pie(fund_grps['Project Count'], labels=fund_grps['Funding Type'], autopct='%1.1f%%', startangle=140)
-  ax2.set_title("Project Distribution by Funding Type")
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.pie(fund_grps['Project Count'], labels=fund_grps['Funding Type'], autopct='%1.1f%%', startangle=140)
+ax2.set_title("Project Distribution by Funding Type")
 
 plt.show()
 
