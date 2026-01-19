d7680046216875c59284a0159d54ad737702dea1
alexfallaw <96636767+alexfallaw@users.noreply.github.com>
2026-01-19 13:10:59 -0600

    did pandas learning

--- base.py
+++ base.py
@@ -1,2 +1,12 @@
-msg = "Roll a dice!"
-print(msg)
+import pandas as pd
+
+c = {
+  "colA": [7, 5, 2, 2, 4],
+  "colB": [9, 40, 3, 9, 67]
+}
+
+tableC = pd.DataFrame(c)
+
+x = tableC["colA", "colB"].mode()[1]
+
+print(x)
